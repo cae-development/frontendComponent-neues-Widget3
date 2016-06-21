fd75b5215df4211227b0f84227458a75aaec9775
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-21 12:38:00 +0200

    MicroserviceCall edited by Thomas Winkler

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -68,7 +68,7 @@ var getContent = function(){
     console.log(data);
     //Also update the html element?
     var files = data.files.map(function(file){ 
-        return file.type + ": " + file.type
+        return file.type + ": " + file.path
     }).join("\n"); 
     $("#textarea").html(files);
   },
